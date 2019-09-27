cc41acb27c3e04c477b7707230c66b80354b2ec0
Miguel Bogota <miguelbogota.rico@outlook.com>
2019-09-27 11:03:54 -0500

    Skill bar added
    A template image was added in the about section, this image it will be a photo of me. Below a skill bar was added and some transitions. However, this still is being worked on since I have to update the timing when it scrolls down and think of a way in order to make the perecentage dynamic.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  skills = [
+    { name: 'HTML/CSS', percentage: '95%' },
+    { name: 'JavaScript', percentage: '85%' },
+    { name: 'Java', percentage: '90%' },
+    { name: 'Angular 8', percentage: '90%' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
