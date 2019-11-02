5ca3a6b468f2627cc59389856becee3b53337148
Miguel Bogota <miguelbogota.rico@outlook.com>
2019-11-02 01:31:33 -0500

    Links, skills and images
    All of the links where fix and should be working as expected, the skills bar is now working with the effect once the window scroll to the div position and the images for the projects show properly.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  initAnimation: boolean = false; // Property to know if the skill bar animation has to start
+
+  // Skills in the skill bar
   skills = [
     { name: 'HTML/CSS', percentage: '95%' },
     { name: 'JavaScript', percentage: '85%' },
@@ -18,5 +21,23 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // Check when to start animation for fill the skills bars
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+
+    // Get information of how far is the top from the skill div
+    const componentPosition = document.getElementsByClassName('skill-list').item(0).getBoundingClientRect().top;
+    const scrollPosition = window.pageYOffset * .7;
+
+    // Validation to start animation if is already in the div
+    if (scrollPosition >= componentPosition) { this.initAnimation = true; }
+
+  }
+
+  // Function to go back to the top
+	goUp() {
+		window.scrollTo(0, document.getElementsByClassName('banner').item(0).clientHeight);
+	}
 
 }
