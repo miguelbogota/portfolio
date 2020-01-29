190dd9fb1f0b7237a465fe66a059f8929a36849e
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-01-29 12:26:56 -0500

    App updated for bootstrap
    The AboutComponent was updated for the responsive design using bootstrap.  This ends the bootstrap update and next steps will be optimization of the app.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, AfterContentInit } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
 import { IData } from 'src/app/core/models/IData';
 
@@ -9,7 +9,6 @@ import { IData } from 'src/app/core/models/IData';
 })
 export class AboutComponent implements OnInit {
 
-  initAnimation: boolean = false; // Property to know if the skill bar animation has to start
   showSpinner: boolean = true; // loading screen validation
 
   data: IData; // Storage about component data
@@ -21,7 +20,6 @@ export class AboutComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-
     this.dataService.get('universal').valueChanges().subscribe(universal => {
       this.universal = universal; // Save date
       // Get the data to show in about
@@ -33,31 +31,16 @@ export class AboutComponent implements OnInit {
           { name: this.data.information5, percentage: this.data.information6 },
           { name: this.data.information7, percentage: this.data.information8 },
           { name: this.data.information9, percentage: this.data.information10 }
-        ]
+        ];
         // Once everything loads stop loading animation
         this.showSpinner = false;
       });
     });
-
   }
 
-  // Check when to start animation for fill the skills bars
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-
-    if (!this.showSpinner) {
-      // Get information of how far is the top from the skill div
-      const componentPosition = document.getElementsByClassName('skill-list').item(0).getBoundingClientRect().top;
-      const scrollPosition = window.pageYOffset * .7;
-
-      // Validation to start animation if is already in the div
-      if (scrollPosition >= componentPosition) { this.initAnimation = true; }
-      }
-    }
-
   // Function to go back to the top
-	goUp() {
-		window.scrollTo(0, document.getElementsByClassName('banner').item(0).clientHeight);
-	}
+  goUp() {
+    window.scrollTo(0, document.getElementsByClassName('banner').item(0).clientHeight);
+  }
 
 }
